4b1b74a0d0a346334bf26891ec672eb8276482ba
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2023-02-20 23:55:46 +0100

    build: create order service

---
+++ order/src/main/java/com/service/order/service/OrderService.java
@@ -0,0 +1,41 @@
+package com.service.order.service;
+
+import com.service.order.dto.OrderLineItemsDto;
+import com.service.order.dto.OrderRequest;
+import com.service.order.model.Order;
+import com.service.order.model.OrderLineItems;
+import com.service.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDotList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
