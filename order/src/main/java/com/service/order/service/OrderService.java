5bea26917ee9af4c74c1dc48606a49422e987a47
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2023-02-21 10:02:49 +0100

    build: there was miss writing of name so i change it.

--- order/src/main/java/com/service/order/service/OrderService.java
+++ order/src/main/java/com/service/order/service/OrderService.java
@@ -21,7 +21,7 @@ public class OrderService {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
-        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDotList()
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
                 .stream()
                 .map(this::mapToDto)
                 .toList();
